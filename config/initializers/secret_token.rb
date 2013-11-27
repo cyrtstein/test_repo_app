7123429b0a7791bad204a9c0ce49fbc948c8c4f0
Curt Steinmetz <steinmetz.curt@gmail.com>
2013-11-27 17:21:22 -0500

    some comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRepoApp::Application.config.secret_token = '6d6f2c3e2e7a839a06ce5933c7dd2ad880ed324bb49e22a9b417048f8a3db78eda319942bce53b8bb50f0cb2546abfd84594c58049d3ddb7a25df299d2feab52'
